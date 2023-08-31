186c4321ecd06ef52927f9507a3faad8ddad2eea
Akshay <akshayparadkar29@gmail.com>
2023-08-31 14:24:00 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DataBinding4';
+  detailsArray = [
+    { id: 1, name: 'Detail 1', description: 'Description for Detail 1' },
+    { id: 2, name: 'Detail 2', description: 'Description for Detail 2' },
+    { id: 3, name: 'Detail 3', description: 'Description for Detail 3' },
+    // ... more details
+  ];
 }
